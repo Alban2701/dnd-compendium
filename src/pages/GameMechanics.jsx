f4ed6e9c86686627516cead40c18c7a4144120ed
Alban GENTA <alban.genta@edu-ecole89.com>
2025-02-10 10:42:22 +0100

    fixed monsters list

--- src/pages/GameMechanics.jsx
+++ src/pages/GameMechanics.jsx
@@ -1,8 +1,10 @@
+import "../styles/WIPPage.css"
+
 const GameMechanics = () => {
     return (
         <>
-            <div className="text-center">
-                <h1>Cette page est en cours de construction</h1>
+            <div className="text-center wip-background-container">
+                <h1 className="content-container">Cette page est en cours de construction</h1>
             </div>
         </>
     )
--- src/pages/Monsters.jsx
+++ src/pages/Monsters.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { Endpoints } from "../_services/endpoints.service"
-import { Accordion, Pagination, InputGroup, Form, Button } from "react-bootstrap"
+import { Accordion, Pagination, InputGroup, Form, Container } from "react-bootstrap"
 import AccordionMonster from "../components/AccordionMonster"
 import LoadingButton from "../components/LoadingButton"
 import "../styles/Monsters.css"
@@ -75,7 +75,7 @@ const Monsters = () => {
                                 onChange={handleInputChange}
                             />
                         </InputGroup >
-                        <Accordion alwaysOpen={false} className="opacity-50 w-100">
+                        <Accordion alwaysOpen={false} flush className="opacity-50 w-100">
                             {currentMonsters.map((monster, index) => {
                                 return (
                                     <div key={index}>
@@ -84,27 +84,42 @@ const Monsters = () => {
                                 );
                             })}
                         </Accordion>
-                        <Pagination>
-                            <Pagination.Prev
-                                onClick={() => setCurrentPage(currentPage - 1)}
-                                disabled={currentPage === 1}
-                            />
-                            {[...Array(totalPages)].map((_, index) => (
+                        <div>
+                            <Pagination className="d-flex justify-content-center mt-1 mb-3">
+                                <Pagination.First
+                                    onClick={() => setCurrentPage(1)}
+                                    disabled={currentPage === 1}
+                                />
+                                <Pagination.Prev
+                                    onClick={() => setCurrentPage(currentPage - 1)}
+                                    disable={currentPage === 1}
+                                />
                                 <Pagination.Item
-                                    key={index + 1}
-                                    active={index + 1 === currentPage}
-                                    onClick={() => setCurrentPage(index + 1)}
-                                >
-                                    {index + 1}
-                                </Pagination.Item>
-                            ))}
-                            <Pagination.Next
-                                onClick={() => setCurrentPage(currentPage + 1)}
-                                disabled={currentPage === totalPages}
-                            />
-                        </Pagination>
+                                    onClick={() => setCurrentPage(currentPage - 2)}
+                                    hidden={currentPage <= 2}
+                                >{currentPage - 2}</Pagination.Item>
+                                <Pagination.Item
+                                    onClick={() => setCurrentPage(currentPage - 1)}
+                                    hidden={currentPage <= 1}>{currentPage - 1}</Pagination.Item>
+                                <Pagination.Item active>{currentPage}</Pagination.Item>
+                                <Pagination.Item
+                                    onClick={() => setCurrentPage(currentPage + 1)}
+                                    hidden={currentPage >= totalPages - 1}>{currentPage + 1}</Pagination.Item>
+                                <Pagination.Item
+                                    onClick={() => setCurrentPage(currentPage + 1)}
+                                    hidden={currentPage >= totalPages - 2}>{currentPage + 2}</Pagination.Item>
+                                <Pagination.Next
+                                    onClick={() => setCurrentPage(currentPage + 1)}
+                                    disabled={currentPage === totalPages}
+                                />
+                                <Pagination.Last
+                                    onClick={() => setCurrentPage(totalPages)}
+                                    disabled={currentPage === totalPages}
+                                />
+                            </Pagination>
+                        </div>
                     </div>
-                </div>
+                </div >
             </>
         )
     }
